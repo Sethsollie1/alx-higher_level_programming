b0c042d777360b686c6f92bc347dda7256901ec4
sethsollie1 <solomon.onuma.a@gmail.com>
2024-05-10 08:39:40 -0700

    Advanced task fixed

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,26 +1,20 @@
 #!/usr/bin/python3
+"""lists the 10 most recent commits on a given GitHub repository.
 """
-lists the 10 most recent commits on a given GitHub repository.
-"""
+import sys
+import requests
 
-if __name__ == '__main__':
-    from requests import get
-    from sys import argv
 
-    repo = argv[1]
-    owner = argv[2]
-    i = 0
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    URL = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
-
-    response = get(URL)
-    json = response.json()
-
-    for element in json:
-        if i > 9:
-            break
-        sha = element.get('sha')
-        author = element.get('commit').get('author').get('name')
-        print("{}: {}".format(sha, author))
-        i += 1
-
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
